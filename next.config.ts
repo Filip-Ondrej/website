08979b19b61e6e737874e6d1f4411b800ab9ec1a
Filip Ondrej <67735611+Filip-Ondrej@users.noreply.github.com>
2025-09-10 21:47:32 +0200

    build: Next15 static export via output:export (remove next export)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    // Make a static export to /out when running `next build`
+    output: 'export',
+    images: { unoptimized: true }, // allows next/image without an image server
 };
 
 export default nextConfig;
